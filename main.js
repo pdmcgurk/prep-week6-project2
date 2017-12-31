69abede0b3481464f266f492ababf1b10eac90af
pdmcgurk <pdmcgurk@gmail.com>
2017-12-30 23:00:06 -0600

    Assignment complete

--- main.js
+++ main.js
@@ -11,42 +11,58 @@
 // Add your JS here.
 
 
-$(document).ready(function() {  
-    
-    
-//1. 
-   
-   
-//2.  
-    
-    
-//3. 
-   
-	    
+$(document).ready(function() {
+
+
+//1.
+   $(".alertme").click(function(){
+     alert("jQuery seems easy so far!");
+   });
+
+//2.
+  $(".clickme").click(function(){
+    $(".clickme").text("I was clicked");
+  });
+
+//3.
+  $(".addStyle").click(function(){
+    $("button").addClass("buttonStyle");
+  });
+
 //4.
- 
-   
-//5. 
+  $(".addDifferentStyle").click(function(){
+    $(".addDifferentStyle").addClass("addUniqueStyle");
+  });
 
-	
+//5.
+  $(".removeBtn").click(function(){
+    $("button").removeClass("buttonStyle");
+    $("button").removeClass("addUniqueStyle");
+  });
+
 //6.
-	
-	
+  $(".clickToggle").click(function(){
+    if ($(".clickToggle").hasClass("buttonStyle")) {
+      $(".clickToggle").toggleClass("buttonStyle");
+    } else {
+      $(".clickToggle").addClass("buttonStyle");
+    }
+  });
+
 //7.
+  $(".squareBtn").click(function(){
+    $(".square").hide();
+  });
 
-	
-	
 //8.
-
-
+  $(".circle").click(function(){
+    $(".circle").hide();
+  });
 
 //9.
+  $(".toggleRedSquare").click(function(){
+    $(".redSquare").toggle(1500);
+  });
 
 
-
-});  // Close: $(document).ready(function() { 
-	
-	
-
-	
-	
+});  // Close: $(document).ready(function() {
